b4dfb15549ed08e8280202acea94a59d2d5dc723
mahade <mahade.adib45@gmail.com>
2024-12-20 22:36:08 +0600

    basic setup with tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,6 +10,28 @@ export default {
   theme: {
     extend: {
       colors: {
+        white: "#ffffff",
+        gray: {
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9da5b4",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+        },
+        blue: {
+          200: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+        },
+        "dark-bg": "#101214",
+        "dark-secondary": "#1d1f21",
+        "dark-tertiary": "#3b3d40",
+        "blue-primary": "#0275ff",
+        "stroke-dark": "#2d3235",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
